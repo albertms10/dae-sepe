93a599856bde23451ee45c5f6f9a2a9c036c82f2
albert <albertmasa2@gmail.com>
2020-08-08 15:58:01 +0200

    feat: Implementa escritura de archivo en generateXML

--- src/helpers/generate-xml.ts
+++ src/helpers/generate-xml.ts
@@ -1,10 +1,19 @@
+import fs from "fs";
+import path from "path";
 import { js2xml } from "xml-js";
 import * as XML from "xml:schema";
 import { prepareXML } from "./index";
 
-export default (Datos: XML.Datos): string =>
-  js2xml(prepareXML(Datos), {
-    compact: true,
-    ignoreComment: true,
-    spaces: 4,
-  });
+export default (Datos: XML.Datos): void => {
+  fs.writeFile(
+    path.resolve(__dirname, "../../out", "output.xml"),
+    js2xml(prepareXML(Datos), {
+      compact: true,
+      ignoreComment: true,
+      spaces: 4,
+    }),
+    (err) => {
+      if (err) console.error(err);
+    }
+  );
+};
