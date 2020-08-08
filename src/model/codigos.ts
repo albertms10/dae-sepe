007aeb79ef08b8938991b5cb2db8de1ecdc75307
albert <albertmasa2@gmail.com>
2020-08-09 01:27:33 +0200

    feat: Añade enumerados de códigos

---
+++ src/model/codigos.ts
@@ -0,0 +1,24 @@
+export const enum Jornada {
+  /** No trabajado */
+  X = "X",
+  /** Descanso */
+  D = "D",
+}
+
+export const enum Periodos_Actividad {
+  /** Días no trabajados en el mes/periodo (días de pago de la prestación) */
+  "01" = "01",
+  /**
+   * Días sin actividad laboral por no acudir al llamamiento
+   * _Fijos discontinuos_ (excluidos del pago de la prestación)
+   */
+  "02" = "02",
+  /** Días trabajados en el mes/periodo */
+  "03" = "03",
+  /** Días IT (excluidos del pago de la prestación por este sistema) */
+  "04" = "04",
+  /** Descanso semanal */
+  "05" = "05",
+  /** Vacaciones anuales retribuidas (excluidas del pago de la prestación) */
+  "06" = "06",
+}
