c2e31c7a06b4077af18ee2a81bbf8d6e7efd5b57
albert <albertmasa2@gmail.com>
2020-08-09 23:49:59 +0200

    refactor: Redistribuye la función de generarSchema y simplifica su uso

---
+++ src/model/calculos/generar-schema.ts
@@ -0,0 +1,40 @@
+import { Info } from "xml:schema";
+import { fechaFinalMes, fechaInicioMes, formatoFecha } from "../../utils";
+import { Meses } from "../codigos";
+import * as Model from "../model";
+import { calculoIntervaloActividad } from "./index";
+
+export default (
+  { Representante, Empresa, Trabajador, Mes }: Info,
+  Horas_Ordinarias: (number | null)[]
+) => {
+  const Fecha = new Date(2020, parseInt(Meses[Mes]));
+
+  return {
+    Datos_Representante: new Model.Representante(
+      Representante.CIF_NIF,
+      Representante.Nombre,
+      Representante.Apellido1
+    ),
+    Datos_Empresa: new Model.Empresa(
+      Empresa.CIF_NIF,
+      Empresa.CCC,
+      Empresa.Numero_ERE
+    ),
+    Datos_Trabajador: new Model.Trabajador(
+      Trabajador.NIF_NIE,
+      new Model.DatosActividad(
+        Trabajador.Coeficiente_Actividad,
+        new Model.PeriodoActividad(
+          formatoFecha(fechaInicioMes(Fecha)),
+          formatoFecha(fechaFinalMes(Fecha)),
+          calculoIntervaloActividad(
+            Trabajador.Jornada_Habitual,
+            Horas_Ordinarias,
+            Fecha
+          )
+        )
+      )
+    ),
+  };
+};
--- src/model/calculos/index.ts
+++ src/model/calculos/index.ts
@@ -2,3 +2,4 @@ export { default as calculoCodigosFinSemana } from "./calculo-codigos-fin-semana
 export { default as calculoCodigosJornada } from "./calculo-codigos-jornada";
 export { default as calculoIntervaloActividad } from "./calculo-intervalo-actividad";
 export { default as codigosAIntervalos } from "./codigos-a-intervalos";
+export { default as generarSchema } from "./generar-schema";
