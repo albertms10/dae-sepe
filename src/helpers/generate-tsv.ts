aa170fb77cc508a7168f6f25ccca57ccf5c4b146
albert <albertmasa2@gmail.com>
2020-08-09 13:59:16 +0200

    docs: Añade comentarios JSDoc a generateTSV

--- src/helpers/generate-tsv.ts
+++ src/helpers/generate-tsv.ts
@@ -1,6 +1,9 @@
 import * as path from "path";
 import * as fs from "fs";
 
+/**
+ * Genera y escribe un archivo TSV con los datos proporcionados.
+ */
 export default (Datos: any[][]) => {
   fs.writeFile(
     path.resolve(__dirname, "../../out", "output.tsv"),
